40cee67294ee5ca04fa4a6e495cb4f5281683706
Ethan <ethan.steip@gmail.com>
2023-04-05 17:11:49 -0400

    have a working grid and triangle pattern

--- coloured-grid.js
+++ coloured-grid.js
@@ -1,29 +1,42 @@
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(700, 1000);
   noLoop();
+}
+
+// Creates a box with 2 different coloured triangles inside
+class Box {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.myColours = ["#f4f1de","#e07a5f","#3d405b","#81b29a","#f2cc8f", "#FFFFFF"];
+    this.randCol1=random(this.myColours.length);
+    this.randCol1=floor(this.randCol1);
+    this.randCol2=random(this.myColours.length);
+    this.randCol2=floor(this.randCol2);
+  }
+  
+  
+  
+  draw() {
+    noStroke();
+    square(this.x, this.y, 50);
+    fill(this.myColours[this.randCol1]);
+    triangle(this.x, this.y, this.x, this.y + 50, this.x + 50, this.y + 50);
+  }
+
 }
 
 function draw() {
   background(200,150,245);
-
+  
   // distance between squares
   let space = 50;
-  strokeWeight(0);
-  
-  // Colours
-  const myColours = ["#f4f1de","#e07a5f","#3d405b","#81b29a","#f2cc8f", "#FFFFFF"];
-  
-  // two for loops are needed to make a grid of squares
-  // the outer loop controls how many rows are made
-  // the inner loop controls how many columns are made
-  for (let y = 0; y < 10; y++) {
 
-    for (let x = 0; x < 10; x++) {
-      randCol=random(myColours.length);
-      randCol=floor(randCol);
-      fill(myColours[randCol]);
-      square(x * space, y * space, 50);
-      
+    for (let y = 0; y < 20; y++) {
+    for (let x = 0; x < 14; x++) {
+      console.log("Coords: ", x * space, y * space);
+      let box = new Box(x * space, y * space);
+      box.draw();
     }
   }
 }
