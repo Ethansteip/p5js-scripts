ee048ce53f3a6abe4f2802ad89fda65ec106486a
Ethan <ethan.steip@gmail.com>
2023-04-05 18:25:15 -0400

    final v1 of triangle pattern

--- coloured-grid.js
+++ coloured-grid.js
@@ -1,5 +1,5 @@
 function setup() {
-  createCanvas(700, 1000);
+  let c = createCanvas(700, 1000);
   noLoop();
 }
 
@@ -8,7 +8,7 @@ class Box {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.myColours = ["#f4f1de","#e07a5f","#3d405b","#81b29a","#f2cc8f", "#FFFFFF"];
+    this.myColours = ["#0d1321","#1d2d44","#3e5c76","#748cab","#f0ebd8"];
     this.randCol1=random(this.myColours.length);
     this.randCol1=floor(this.randCol1);
     this.randCol2=random(this.myColours.length);
@@ -18,12 +18,21 @@ class Box {
   
   
   draw() {
+    
+    let randNum = random(2);
+    randNum = floor(randNum);
+    console.log(randNum);
+
     noStroke();
     square(this.x, this.y, 50);
-    fill(this.myColours[this.randCol1]);
-    triangle(this.x, this.y, this.x, this.y + 50, this.x + 50, this.y + 50);
+    if (randNum === 1) {
+      fill(this.myColours[this.randCol1]);
+      triangle(this.x + 50, this.y, this.x, this.y, this.x, this.y + 50);
+    } else {
+      fill(this.myColours[this.randCol2]);
+      triangle(this.x, this.y, this.x, this.y + 50, this.x + 50, this.y + 50);
+    }
   }
-
 }
 
 function draw() {
@@ -33,10 +42,12 @@ function draw() {
   let space = 50;
 
     for (let y = 0; y < 20; y++) {
-    for (let x = 0; x < 14; x++) {
-      console.log("Coords: ", x * space, y * space);
-      let box = new Box(x * space, y * space);
-      box.draw();
+      for (let x = 0; x < 14; x++) {
+        console.log("Coords: ", x * space, y * space);
+        let box = new Box(x * space, y * space);
+        box.draw();
     }
   }
+  
+  save("others.png");
 }
